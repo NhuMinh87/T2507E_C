cffc0230dc71ac87b891185b172245d7d16c95dc
NhuMinh87 <minhnguyennhu87@gmail.com>
2025-09-05 15:52:22 +0700

    Add files via upload

---
+++ asm51.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void tim_uoc_va_tinh_tong(int n) {
+    int tong = 0;
+    printf("Cac uoc cua %d la: ", n);
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+            tong += i;
+        }
+    }
+    printf("\nTong cac uoc cua %d la: %d\n", n, tong);
+}
+
+int main() {
+    int n;
+    printf("Nhap so nguyen n: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("So %d khong hop le.\n", n);
+    } else {
+        tim_uoc_va_tinh_tong(n);
+    }
+
+    return 0;
